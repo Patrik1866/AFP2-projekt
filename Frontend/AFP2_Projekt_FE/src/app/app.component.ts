a5df7ca3775157e7ed187a3264e6c3be4125d837
Bárdos Botond <bardosb02@gmail.com>
2024-03-19 11:24:01 +0100

    Login/Register form alap fájlok

--- Frontend/AFP2_Projekt_FE/src/app/app.component.ts
+++ Frontend/AFP2_Projekt_FE/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet, HttpClientModule, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
