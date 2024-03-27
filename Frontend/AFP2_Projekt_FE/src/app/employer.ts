0408a428c324cdeb390dadd599f2da2f0cb475cd
Bárdos Botond <bardosb02@gmail.com>
2024-03-27 11:18:13 +0100

    Checkbox for register DEMO
    Nem végleges, nem minden működik rendesen, rossz API-ba tolja fel az adatokat, ki kell javítani még.

---
+++ Frontend/AFP2_Projekt_FE/src/app/employer.ts
@@ -0,0 +1,13 @@
+export class Employer {
+    constructor(
+        public id?: number,
+        public name?: string,
+        public password?: string,
+        public email?: string,
+        public job?: string,
+        public phone?: string,
+        public picture?: string,
+        public userCode?: string
+      ) { }
+  }
+  
--- Frontend/AFP2_Projekt_FE/src/app/register/register.component.ts
+++ Frontend/AFP2_Projekt_FE/src/app/register/register.component.ts
@@ -8,49 +8,80 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { FormControl, Validator, Validators } from '@angular/forms';
 import { error } from 'node:console';
+import { Employer } from '../employer';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [RouterModule, CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
   newUser: User = new User();
-  
+  newEmployer: Employer = new Employer();
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
-  register(event: Event, name: string, password: string, email: string, job: string, phone: string, picture: string, userCode: string): void {
+  register(
+    event: Event,
+    name: string,
+    password: string,
+    email: string,
+    job: string,
+    phone: string,
+    picture: string,
+    userCode: string,
+    accountType: string
+  ): void {
     event.preventDefault();
     if (password.length >= 5 && /\d/.test(password)) {
       this.newUser.password = password;
     } else {
-      window.alert("Invalid password");
+      window.alert('Invalid password');
       return;
     }
     if (name.length >= 4) {
       name = name.charAt(0).toUpperCase() + name.slice(1);
       this.newUser.name = name;
     } else {
-      window.alert("Wrong format for the name");
+      window.alert('Wrong format for the name');
       return;
     }
-    this.newUser.email = email;
-    this.newUser.job = job;
-    this.newUser.phone = phone;
-    this.newUser.picture = picture;
-    this.newUser.userCode = userCode;
 
-    this.addUser(this.newUser).subscribe(user => {
-      window.alert('User registered successfully');
-      console.log('User registered successfully');
-      this.router.navigate(['/login']);
-    });
+    if (accountType === 'standard') {
+      this.newUser.email = email;
+      this.newUser.job = job;
+      this.newUser.phone = phone;
+      this.newUser.picture = picture;
+      this.newUser.userCode = userCode;
+      this.addUser(this.newUser, '/users').subscribe((user) => {
+        window.alert('User registered successfully');
+        console.log('User registered successfully');
+        this.router.navigate(['/login']);
+      });
+    } else if (accountType === 'employer') {
+      this.newEmployer.email = email;
+      this.newEmployer.job = job;
+      this.newEmployer.phone = phone;
+      this.newEmployer.picture = picture;
+      this.newEmployer.userCode = userCode;
+      this.addEmployer(this.newEmployer, '/employers').subscribe((employer) => {
+        window.alert('Employer registered successfully');
+        console.log('Employer registered successfully');
+        this.router.navigate(['/login']);
+      });
+    }
   }
 
-  addUser(user: User): Observable<User> {
-    return this.http.post<User>('http://localhost:8080/afp2API/users', user);
+  addUser(user: User, endpoint: string): Observable<User> {
+    return this.http.post<User>(`http://localhost:8080/afp2API/users`, user);
+  }
+
+  addEmployer(employer: Employer, endpoint: string): Observable<Employer> {
+    return this.http.post<Employer>(
+      `http://localhost:8080/afp2API/employers`,
+      employer
+    );
   }
 }
