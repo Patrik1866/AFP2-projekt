2a99bf5041e2d9f31ff4a7079f1c4e821e2c70cc
Bárdos Botond <bardosb02@gmail.com>
2024-04-04 12:23:38 +0200

    Job page alapok, demo-hoz

--- Frontend/AFP2_Projekt_FE/src/app/jobpage/jobpage.component.ts
+++ Frontend/AFP2_Projekt_FE/src/app/jobpage/jobpage.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-jobpage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './jobpage.component.html',
   styleUrl: './jobpage.component.scss'
 })
-export class JobpageComponent {
+export class JobpageComponent implements OnInit {
+  jobs = [
+    {company: 'Company 1', jobTitle: 'Job Title 1', jobDescription: 'Job Description 1', salary: 'Salary 1'},
+    {company: 'Company 2', jobTitle: 'Job Title 2', jobDescription: 'Job Description 2', salary: 'Salary 2'},
+    {company: 'Company 3', jobTitle: 'Job Title 3', jobDescription: 'Job Description 3', salary: 'Salary 3'},
+    {company: 'Company 4', jobTitle: 'Job Title 4', jobDescription: 'Job Description 4', salary: 'Salary 4'},
+    {company: 'Company 5', jobTitle: 'Job Title 5', jobDescription: 'Job Description 5', salary: 'Salary 5'},
+    {company: 'Company 6', jobTitle: 'Job Title 6', jobDescription: 'Job Description 6', salary: 'Salary 6'},
+    // Add more jobs as needed
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
